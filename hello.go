1ebf6d3ad353e2fbbb3d2701c02344c40ce29893
PangPangGod <rhcp1134@gmail.com>
2025-03-21 23:47:18 +0900

    1. Create first module on GO

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("This is New Learning-Go Project with my dirty codes")
+	fmt.Println("-", quote.Go())
+	fmt.Println("-", quote.Glass())
+}
